1de153524a7319361a6ebcde604152ce109d0a9f
Jakub Mikula <jakub.mikula@lhsystems.com>
2023-09-01 19:25:15 +0200

    Init

---
+++ src/main/java/com/jakubmikula/dibulb/DiBulbApplication.java
@@ -0,0 +1,13 @@
+package com.jakubmikula.dibulb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiBulbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiBulbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jakubmikula/dibulb/DiBulbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakubmikula.dibulb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiBulbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
